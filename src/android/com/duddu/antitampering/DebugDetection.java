551b5cd81134fa19c2b5238beb17c36797d72fe2
sanqbear <sanqbear@outlook.com>
2025-05-13 09:34:17 +0900

    fix(android): update DebugDetection to use application context

--- src/android/com/duddu/antitampering/DebugDetection.java
+++ src/android/com/duddu/antitampering/DebugDetection.java
@@ -1,22 +1,23 @@
 package com.duddu.antitampering;
 
 import android.os.Debug;
+import android.content.Context;
 
 import java.lang.reflect.Field;
 
 
 class DebugDetection {
 
-    public static void check(String packageName) throws Exception {
+    public static void check(Context context) throws Exception {
         if (hasDebuggerAttached()) {
             throw new Exception("Debugger attached");
-        } else if (getDebugField(packageName)) {
+        } else if (getDebugField(context)) {
             throw new Exception("App running in Debug mode");
         }
     }
 
-    private static Boolean getDebugField(String packageName) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
-        Class<?> buildConfigClass = Class.forName(packageName.concat(".BuildConfig"));
+    private static Boolean getDebugField(Context context) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        Class<?> buildConfigClass = Class.forName(context.getPackageName().concat(".BuildConfig"));
         Field debugField = buildConfigClass.getField("DEBUG");
         return debugField.getBoolean(null);
     }
